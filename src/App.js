e67a691ee47f16fbbe2d78a5c97f778fc9d49168
Denis <zvlatarevich.d@gmail.com>
2023-03-19 23:39:58 +0300

    init

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <Header />
         <Routes>
             <Route index element={<ProductList />}/>
-            <Route patch={'form'} element={<Form />}/>
+            <Route path={'form'} element={<Form />}/>
         </Routes>
     </div>
   );
